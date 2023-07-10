1b9eec03ac5bbb40af9eb745c0745cbb7b85361f
tresor <tshims79@gmail.com>
2023-07-10 16:26:52 +0200

    Add project files.

---
+++ WithoutOCPDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WithoutOCPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
